952347c285de1741790f57bd8b2810fac6d4b21d
Kamasanisailaja <106640335+Kamasanisailaja@users.noreply.github.com>
2023-05-25 17:33:12 +0530

    Add files via upload

---
+++ 3b.py
@@ -0,0 +1,18 @@
+a=int(input("enter a value of a:"))
+b=int(input("enter a value of b:"))
+c=int(input("enter a value of c:"))
+if a>b:
+   if a>c:
+      {
+         print("a is greater")
+      }
+   else:
+         print("c is graeter")
+
+else:
+     if b>c:
+        {
+            print("b is greater")
+        }
+     else:
+        print("c is graeter")
